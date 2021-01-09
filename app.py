93e4533496fa9c3b32ff7b17895af253ccb002f5
Kirsty Chatterton <chattertonkirsty@gmail.com>
2021-01-09 15:51:05 +0000

    add materialize cards to dictionary

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
-from flask import (Flask, flash, render_template,
-                   redirect, request, session, url_for)
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
@@ -15,10 +16,18 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
+# Render Home Page
+
+# @app.route("/home")
+# def home():
+#     return render_template('index.html')
+
+# Render Dictionary
+
 
 @app.route("/")
-@app.route("/get_dictionary")
-def get_dictionary():
+@app.route("/dictionary")
+def dictionary():
     dictionary = mongo.db.dictionary.find()
     return render_template("dictionary.html", dictionary=dictionary)
 
