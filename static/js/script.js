293e17a906b561d0e4f0b21ce649a7d4bcdf2872
Kirsty Chatterton <chattertonkirsty@gmail.com>
2021-01-29 22:56:01 +0000

    add password confirmation on sign up template

--- static/js/script.js
+++ static/js/script.js
@@ -18,4 +18,13 @@ $(document).ready(function () {
 
     //Sort By Dropdown
     $('.dropdown-trigger').dropdown();
+
+    //Code is from Stack Overflow for confirm Password:
+    //https://stackoverflow.com/questions/21727317/how-to-check-confirm-password-field-in-form-without-reloading-page
+    $('#password, #confirm_password').on('keyup', function () {
+        if ($('#password').val() == $('#confirm_password').val()) {
+            $('#message').html('Passwords Match').css('color', 'green');
+        } else
+            $('#message').html('Passwords Do Not Match').css('color', 'red');
+    });
 });
