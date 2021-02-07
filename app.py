eed35d96098da338a2ba1dea511386c17d9c26de
Kirsty Chatterton <chattertonkirsty@gmail.com>
2021-02-07 23:22:02 +0000

    add custom 500 error page

--- app.py
+++ app.py
@@ -225,6 +225,10 @@ def contact():
 def page_not_found(e):
     return render_template("404.html"), 404
 
+@app.errorhandler(500)
+# 500 Error Handler
+def internal_server_error(e):
+    return render_template("500.html"), 500
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
